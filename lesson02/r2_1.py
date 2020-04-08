8cd41a3cff44dc708b2544da8949818fbdfbcbbe
Matous Holinka <matous.holinka@nic.cz>
2020-04-08 11:29:27 +0200

    master: folder was renamed

---
+++ lesson02/r2_1.py
@@ -0,0 +1,11 @@
+# 2.1 Review task
+# Promenne s inputem pro uzivatele
+JMENO = input('VLOZTE JMENO: ')
+PRIJMENI = input('VLOZTE PRIJMENI: ')
+
+# Vytvorime novou promennou
+cele_jmeno = JMENO + ' ' + PRIJMENI
+
+# Vypiseme novou promennou
+print(cele_jmeno)
+
