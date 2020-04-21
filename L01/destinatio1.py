5b99d80b837567e0944e37d3a0684d1315ee9a65
Matous Holinka <matous.holinka@nic.cz>
2020-04-21 09:12:15 +0200

    dev: minor fix

--- L01/destinatio1.py
+++ L01/destinatio1.py
@@ -3,8 +3,8 @@
 
 # I. KROK:
 # Definujeme promenne, se kterymi chceme pracovat
-SEZNAM_MEST = ("Praha", "Viden", "Olomouc", "Svitavy", "Zlin", "Ostrava")
-SEZNAM_CEN = (150, 200, 120, 120, 100, 180)
+MESTA = ("Praha", "Viden", "Olomouc", "Svitavy", "Zlin", "Ostrava")
+CENY = (150, 200, 120, 120, 100, 180)
 ODDELOVAC = "=" * 35
 
 # II. KROK:
@@ -27,21 +27,21 @@ print(ODDELOVAC)
 
 # III. KROK:
 # Vyzadame si od uzivatele jednotlive vstupy
-# Cislo destinace, jmeno, prijmeni, vek, email, heslo
+# Cislo destinace, jmeno, prijmeni, rok_narozeni, email, heslo
 por_cislo = int(input("Vyberte cislo lokality: "))
 jmeno = input("JMENO: ")
 prijmeni = input("PRIJMENI: ")
-vek = int(input("VEK: "))
+rok_narozeni = int(input("ROK NAROZENI: "))
 email = input("EMAIL: ")
 heslo = input("HESLO: ")
 print(ODDELOVAC)
 
 # IV. KROK:
 # Modifikujeme tyto hodnoty,
-destinace = SEZNAM_MEST[por_cislo - 1]  # --> destiance[0] pokud bude por_cislo = 1
-cena = SEZNAM_CEN[por_cislo - 1]
+destinace = MESTA[por_cislo - 1]  # --> destiance[0] pokud bude por_cislo = 1
+cena = CENY[por_cislo - 1]
 
-# VI. KROK:
+# V. KROK:
 # Vystupni sekce, vypisujeme konkretni udaje
 # Jmeno, destinaci, cenu, email
 print("UZIVATEL: " + jmeno)
