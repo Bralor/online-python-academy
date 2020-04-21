08e2e41b68fe3aef732899ba399ce3ff5372ba37
Matous Holinka <matous.holinka@nic.cz>
2020-04-21 09:12:26 +0200

    dev: duplicates removed

--- L02/r2_2.py
+++
@@ -1,7 +0,0 @@
-# 2.2 Review task
-# Promenna, ktera obsahuje delku zadaneho retezce
-DELKA_SL = len('quetzalcoatl')
-
-# Vypis promennou s doplnujicim textem
-print('Delka slova "quetzalcoatl" je '+ str(DELKA_SL))
-
